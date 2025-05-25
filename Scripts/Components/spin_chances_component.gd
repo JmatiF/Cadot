a5e446335de25745a06f24f0ebb17eaec423b184
MatiasFernandez <josematiasfernandez5@gmail.com>
2025-05-24 22:04:56 -0300

    Added MainScreen, CardPanel, SpinChancesComponent

---
+++ Scripts/Components/spin_chances_component.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const CARD_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # 1-6 clasics, 7-8 wildcards, 9 bonuses
+const CARD_PROBABILITIES = {
+	1: 0.16,
+	2: 0.16,
+	3: 0.16,
+	4: 0.16,
+	5: 0.16,
+	6: 0.16,
+	7: 0.075,
+	8: 0.075,
+	9: 0.05
+}
---
+++ Scripts/card_panel.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var column_1 := [
+	$"Slots/IconSlot",
+	$"Slots/IconSlot2",
+	$"Slots/IconSlot3"
+]
+
+@onready var column_2 := [
+	$"Slots/IconSlot4",
+	$"Slots/IconSlot5",
+	$"Slots/IconSlot6"
+]
+
+@onready var column_3 := [
+	$"Slots/IconSlot7",
+	$"Slots/IconSlot8",
+	$"Slots/IconSlot9"
+]
+
+@onready var column_4 := [
+	$"Slots/IconSlot10",
+	$"Slots/IconSlot11",
+	$"Slots/IconSlot12"
+]
+
+@onready var column_5 := [
+	$"Slots/IconSlot13",
+	$"Slots/IconSlot14",
+	$"Slots/IconSlot15"
+]

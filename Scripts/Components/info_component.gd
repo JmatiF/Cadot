e404376e01358141e265d5c8e442f3ccd89ca9f5
MatiasFernandez <josematiasfernandez5@gmail.com>
2025-06-11 19:53:53 -0300

    Spanish info Done

--- Scripts/Components/info_component.gd
+++ Scripts/Components/info_component.gd
@@ -1,24 +1,52 @@
 extends Node
 
 @onready var spanish_canvas : CanvasLayer = $"SpanishCanvas"
-var spanish_pages : Array = [0,1]
+var spanish_pages : Array = [1,2,3]
+
+var current_page : int = 0
+
+var current_canvas : CanvasLayer
+var current_array : Array
 
 func _ready() -> void:
 	close()
 
 func display(lenguaje : String):
 	if lenguaje == "Spanish":
-		if show_or_next(spanish_canvas):
-			pass
-			#next()
+		current_canvas = spanish_canvas
+		current_array = spanish_pages
+		show_interface()
+
+func next():
+	if not current_canvas or not current_array:
+		return
+	
+	if current_page >= current_array.size():
+		current_canvas.visible = false
+		current_page = 0 
+		return
+	
+	var page_name := "Page" + str(current_array[current_page])
+	var page := current_canvas.get_node_or_null(page_name)
+	
+	hide_all_pages()
+	page.visible = true
+	current_page += 1
 
 
 func close():
 	spanish_canvas.visible = false
 
-func show_or_next(canvas : CanvasLayer) -> bool:
-	if canvas.visible == true:
-		return true
-	else:
-		spanish_canvas.visible = true
-		return false
+func show_interface():
+	if not current_canvas.visible:
+		current_canvas.visible = true
+	
+	next()
+
+func hide_all_pages() -> void:
+	for i in range(current_array.size()):
+		var page_name := "Page" + str(current_array[i])
+		var page_node := current_canvas.get_node_or_null(page_name)
+		if page_node:
+			page_node.visible = false
+	
--- Scripts/Info/spanish_info.gd
+++ Scripts/Info/spanish_info.gd
@@ -2,5 +2,16 @@ extends CanvasLayer
 
 @onready var info : Node = $"InfoComponent"
 
+func _ready() -> void:
+	display()
+
 func display():
 	info.display("Spanish")
+
+
+func _on_next_button_pressed() -> void:
+	info.next()
+
+
+func _on_prev_button_pressed() -> void:
+	pass # Replace with function body.
